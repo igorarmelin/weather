ec88c61ca37d05cac159c968a43e2fefb83a3659
igorarmelin <igor_armelin@hotmail.com>
2022-07-29 20:58:48 -0300

    added global style

---
+++ src/commons/styles/globalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: #31255a;
+    font-family: Roboto, Helvetica, Arial, sans-serif
+  }
+`
+
+export default GlobalStyles
