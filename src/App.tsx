8c9f6cccf88ebbfa398e6347c5af3622e5218690
igorarmelin <igor_armelin@hotmail.com>
2022-07-29 21:02:17 -0300

    changes on app and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App () {
-  return <h1>Olá!</h1>
-}
+import React from 'react'
+import { QueryClientProvider } from 'react-query'
+import Routes from './routes'
+import { queryClient } from './resources/api/queryClient'
+import GlobalStyles from './commons/styles/globalStyles'
+
+const App = () => (
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyles />
+      <Routes />
+    </QueryClientProvider>
+  </React.StrictMode>
+)
 
 export default App
